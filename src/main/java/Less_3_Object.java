4ddef7caed5f688d1b176458e570ca45c15b7b59
*Egor <egorik13@gmail.com>
2023-06-30 18:34:43 +0300

    less_3_Object

---
+++ src/main/java/Less_3_Object.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Less_3_Object {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+//        ArrayList list = new ArrayList();
+        list.add(2809);
+//        list.add("1234");
+
+        for (Object o : list) {
+            System.out.println(o);
+        }
+    }
+}
