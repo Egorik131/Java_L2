b72881041c1b980fe5ee7eae5056537879ed3a36
*Egor <egorik13@gmail.com>
2023-08-18 19:27:45 +0300

    upd: oop hw7 pattern final

--- src/main/java/oop/hw7/calc/pattern/CalcModel.java
+++ src/main/java/oop/hw7/calc/pattern/CalcModel.java
@@ -1,27 +1,76 @@
 package oop.hw7.calc.pattern;
 
-/**
- * Solid 1. Класс CalcMathFunc отвечает только за одну зону ответственности (арифметические вычисления)
- */
-class CalcMathFunc {
+abstract class CalcMathFunc {
+    double value1, value2;
 
-    public double sum(double value1, double value2) {
+    abstract double getResult();
+}
+
+/**
+ * Solid 1. Класс Sum отвечает только за суммирование
+ */
+class Sum extends CalcMathFunc {
+    @Override
+    double getResult() {
         return value1 + value2;
     }
+}
 
-    public double div(double value1, double value2) {
+/**
+ * Solid 1. Класс Div отвечает только за деление
+ */
+class Div extends CalcMathFunc {
+    @Override
+    double getResult() {
         if (value2 != 0) {
             return value1 / value2;
         } else {
             throw new IllegalArgumentException("Деление на ноль недопустимо.");
         }
     }
+}
 
-    public double mul(double value1, double value2) {
+/**
+ * Solid 1. Класс Mul отвечает только за умножение
+ */
+class Mul extends CalcMathFunc {
+    @Override
+    double getResult() {
         return value1 * value2;
     }
+}
 
-    public double sub(double value1, double value2) {
+/**
+ * Solid 1. Класс Sub отвечает только за Вычитание
+ */
+class Sub extends CalcMathFunc {
+    @Override
+    double getResult() {
         return value1 - value2;
+    }
+}
+
+
+class OperationFactory {
+    public CalcMathFunc createOperator(char ch) {
+        CalcMathFunc operator = null;
+        switch (ch) {
+            case '+':
+                operator = new Sum();
+                break;
+            case '-':
+                operator = new Div();
+                break;
+            case '/':
+                operator = new Mul();
+                break;
+            case '*':
+                operator = new Sub();
+                break;
+            default:
+                System.out.println("Введена неверная операция!");
+                break;
+        }
+        return operator;
     }
 }
