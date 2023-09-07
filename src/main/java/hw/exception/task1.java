996855153e678b6713773ba24ae569580c537e6b
*Egor <egorik13@gmail.com>
2023-09-08 01:16:31 +0300

    upd: hw2 exception

--- src/main/java/hw/exception/task1.java
+++ src/main/java/hw/exception/task1.java
@@ -11,20 +11,19 @@ import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-        double number = getDoubleInput("Введите дробное число: ");
-        System.out.println("Вы ввели число: " + number);
+        float num = getFloatInput("Введите дробное число: ");
+        System.out.println(num);
     }
 
-    public static double getDoubleInput(String prompt) {
+    public static float getFloatInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print(prompt);
             try {
-                return Double.parseDouble(scanner.nextLine());
+                return Float.parseFloat(scanner.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("Некорректный ввод. Пожалуйста, введите дробное число.");
             }
         }
     }
-
 }
