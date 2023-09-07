bd65d3444f16ce39a6acde84f656b8e40d33de58
*Egor <egorik13@gmail.com>
2023-09-08 00:52:12 +0300

    upd hw7

--- src/main/java/oop/hw7/calc/pattern/Main.java
+++ src/main/java/oop/hw7/calc/pattern/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         CalcMathFunc operator;
-        operator = OperationFactory.createOperation('/');
+        operator = OperationFactory.createOperation('+');
         operator.value1 = 3;
         operator.value2 = 4;
         System.out.println(operator.getResult());
